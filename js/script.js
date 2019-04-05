0ed0a668f11834e00763a0225ae72ebdbc849c02
thabang <codeadvocate@gmail.com>
2019-04-05 02:38:43 +0200

    About Header And Slider

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ $(window).on('load', function () { // makes sure that whole site is loaded
     $('#status').fadeOut();
     $('#preloader').delay(350).fadeOut('slow');
 });
+
+$(function () {
+    $('#team-members').owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+    });
+})
+

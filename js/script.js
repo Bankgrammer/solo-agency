9775e9b677242352cfe46259d71065ab245c1cbd
Bankgrammer <codeconstable@gmail.com>
2019-04-09 00:54:24 +0200

    Skills Section Final Commit

--- js/script.js
+++ js/script.js
@@ -20,3 +20,24 @@ $(function () {
     });
 })
 
+/* =========================================
+                Progress Bars
+============================================ */
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+
+        });
+
+        this.destroy();
+    }, {
+            offset: 'bottom-in-view'
+        });
+
+});
